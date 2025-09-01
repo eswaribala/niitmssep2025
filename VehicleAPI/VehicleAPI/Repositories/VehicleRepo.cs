0104e18d7347137835358a5e67f88ab5d6e7104d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-01 14:51:20 +0530

    updated

---
+++ VehicleAPI/VehicleAPI/Repositories/VehicleRepo.cs
@@ -0,0 +1,32 @@
+﻿using VehicleAPI.Models;
+
+namespace VehicleAPI.Repositories
+{
+    public class VehicleRepo : IVehicleRepo
+    {
+        public Task<Vehicle> AddVehicle(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vehicle> GetVehicle(string vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Vehicle>> GetVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveVehicle(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vehicle> UpdateVehicle(string RegNo, string color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
