7fda97581bfd6915ad760635a8abfb9e7c239d20
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-01 16:02:31 +0530

    updated

--- VehicleAPI/VehicleAPI/Program.cs
+++ VehicleAPI/VehicleAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using VehicleAPI.Contexts;
@@ -21,9 +23,51 @@ builder.Services.AddTransient<IVehicleRepo,VehicleRepo>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "VehicleAPI", Version = "v1" });
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "TraderAPI", Version = "v1" });
 
 
+});
+builder.Services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new ApiVersion(1, 0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                    new HeaderApiVersionReader("x-api-version"),
+                                                    new MediaTypeApiVersionReader("x-api-version"));
+});
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+var policyName = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+                      builder =>
+                      {
+                          builder
+                             .WithOrigins("http://localhost:*", "")
+                             //.WithOrigins("http://localhost:3000")
+                             // specifying the allowed origin
+                             // .WithMethods("GET") // defining the allowed HTTP method
+                             .AllowAnyOrigin()
+                             // .WithHeaders(HeaderNames.ContentType, "ApiKey")
+                             .AllowAnyMethod()
+                            .AllowAnyHeader(); // allowing any header to be sent
+                      });
+});
+
+builder.Services.AddAntiforgery(o => o.HeaderName = "X-CSRF-TOKEN");
+
+// Use ControllersWithViews to bring in ViewFeatures & antiforgery filter support
+builder.Services.AddControllersWithViews(options =>
+{
+    // Add the filter only in non-dev to avoid breaking Swagger
+    if (!builder.Environment.IsDevelopment())
+        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
 });
 
 var app = builder.Build();
@@ -42,5 +86,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+//Apply migrations at startup (nice for containers/dev)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<VehicleContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
