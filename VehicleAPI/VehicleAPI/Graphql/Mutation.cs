79a2af16528b2fdc90af2eaf873e7d655b601b4f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-02 10:16:18 +0530

    updated

--- VehicleAPI/VehicleAPI/Graphql/Mutation.cs
+++ VehicleAPI/VehicleAPI/Graphql/Mutation.cs
@@ -1,6 +1,29 @@
-﻿namespace VehicleAPI.Graphql
+﻿using VehicleAPI.Contexts;
+using VehicleAPI.DTO;
+using VehicleAPI.Models;
+
+namespace VehicleAPI.Graphql
 {
     public class Mutation
     {
+
+        public async Task<Vehicle> CreateVehicle([Service] VehicleContext db, VehicleDTO input)
+        {
+            var vehicle = new Vehicle
+            {
+                RegistrationId = input.RegistrationId,
+                Maker = input.Maker,
+                DOR = input.DOR,
+                ChassisNo = input.ChassisNo,
+                EngineNo = input.EngineNo,
+                Color = input.Color,
+                FuelType = input.FuelType
+               
+            };
+            db.Vehicles.Add(vehicle);
+            db.SaveChangesAsync();
+            return vehicle;
+        }
+
     }
 }
