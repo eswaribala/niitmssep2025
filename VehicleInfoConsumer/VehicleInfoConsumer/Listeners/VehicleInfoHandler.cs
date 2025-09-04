7dac46c419bdfc9178efb488e04089f837c20eb2
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-04 12:35:13 +0530

    updated

--- VehicleInfoConsumer/VehicleInfoConsumer/Listeners/VehicleInfoHandler.cs
+++ VehicleInfoConsumer/VehicleInfoConsumer/Listeners/VehicleInfoHandler.cs
@@ -1,11 +1,18 @@
 ﻿using Confluent.Kafka;
 using System.Text.Json;
 using VehicleInfoConsumer.DTOs;
+using VehicleInfoConsumer.Repository;
 
 namespace VehicleInfoConsumer.Listeners
 {
     public class VehicleInfoHandler : IMessageHandler
     {
+        private readonly IVehicleRepo repo;
+
+        public VehicleInfoHandler(IVehicleRepo repo)
+        {
+            this.repo = repo;
+        }
         public Task<bool> HandleAsync(string topic, string key, string value, Headers headers, CancellationToken ct)
         {
             // Example: deserialize and do something
@@ -14,6 +21,18 @@ namespace VehicleInfoConsumer.Listeners
                 var vehicle = JsonSerializer.Deserialize<VehicleReadDTO>(value) ?? null;
                 Console.WriteLine($"[VehicleInfoHandler] topic={topic} key={key} id={vehicle.RegNo} model={vehicle.model} dor={vehicle.dor}");
                 // todo: call your domain logic / DB, etc.
+                VehicleBson vehicleBson = new VehicleBson
+                {
+                    RegNo = vehicle.RegNo,
+                    Model = vehicle.model,
+                    DOR = vehicle.dor,
+                    ChassisNo = vehicle.chassisNo,
+                    EngineNo = vehicle.engineNo,
+                    Color = vehicle.color,
+                    FuelType = vehicle.fuelType
+                };
+                repo.AddVechicle(vehicleBson);
+
                 return Task.FromResult(true); // commit
             }
             catch (Exception ex)
